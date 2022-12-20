c44555b614519b8346375758beb33e77ed6cc811
Mathias Lorenzen <mathias.lorenzen@live.com>
2022-12-20 11:38:32 +0100

    Fix for linter.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "plugin:@typescript-eslint/recommended"
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
